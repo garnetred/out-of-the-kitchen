2020423c1a09ba1fd14fdf65b0a7fefd0d5675f2
December G <dgarnetsmith@gmail.com>
2020-04-01 22:59:10 -0600

    Fix error with variables not being declared propertly and leaking into global scope

--- test/ingredients-test.js
+++ test/ingredients-test.js
@@ -1,18 +1,16 @@
 const chai = require('chai');
 const expect = chai.expect;
-const userData = '../data/users.js';
 
 const Ingredient = require('../src/Ingredient');
 
 describe('Ingredient', function() {
 
-  let user1, ingredient1;
+  let ingredient1;
 
   beforeEach(function() {
-    // user1 = new User({name: "Saige O'Kon", id: 1, pantry: [{ingredient: 11477, amount: 4}, {ingredient: 11297, amount: 4}]});
     ingredient1 = new Ingredient({id: 20081,
-    name: "wheat flour",
-    estimatedCostInCents: 142});
+      name: "wheat flour",
+      estimatedCostInCents: 142});
   });
 
   it('should be a function', function() {
@@ -20,7 +18,7 @@ describe('Ingredient', function() {
   });
 
   it('should be an instance of the Ingredient class', function() {
-    expect(ingredient1).to.be.an.instanceof(Ingredient);;
+    expect(ingredient1).to.be.an.instanceof(Ingredient);
   });
 
 });
